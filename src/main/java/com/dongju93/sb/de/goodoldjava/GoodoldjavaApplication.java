4fea2a4e2e595a7589da693ffbf2acddc3b72d80
dongju93 <spdlqj011@gmail.com>
2026-04-22 00:16:16 +0900

    :tada: Java is still good so I have to learn again

---
+++ src/main/java/com/dongju93/sb/de/goodoldjava/GoodoldjavaApplication.java
@@ -0,0 +1,13 @@
+package com.dongju93.sb.de.goodoldjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodoldjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodoldjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dongju93/sb/de/goodoldjava/GoodoldjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongju93.sb.de.goodoldjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodoldjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
